9ef773497b438777710f727119a89acb0bd9b8e9
Sam Therapy <sam@samtherapy.net>
2022-02-06 14:46:27 -0600

    Add Misskey support
    Signed-off-by: Sam Therapy <sam@samtherapy.net>

---
+++ src/build-styles.js
@@ -0,0 +1,22 @@
+// build the styles
+import { readFileSync, writeFileSync } from "fs";
+import { renderSync } from "node-sass";
+
+var staticDir = "./src/public/";
+var srcDir = "./src/stylesrc/";
+var themes = ["masto-light","masto-dark","masto-auto"];
+
+
+themes.forEach(function(s){
+    var sassFile = srcDir+s+".scss";
+    var cssFile = staticDir+s+".css";
+    var result = renderSync({
+        data: readFileSync(sassFile,"utf8"),
+        includePaths:[srcDir]
+    });
+
+    writeFileSync(cssFile,result.css,"utf8");
+
+});
+
+console.log("ok");
--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 // do a test
 
 
-import { createReadStream, writeFileSync } from 'fs';
-import convert from '../lib/convert.js';
+import { createReadStream, writeFileSync } from "fs";
+import convert from "../lib/convert.js";
 
 	
-var r = createReadStream('./test/sample.atom');
+var r = createReadStream("./test/sample.atom");
 	
 convert(r,function(er,data){
-  if (er){return console.log('error: ',er)}
-  console.log('ok');
-  writeFileSync('./test/result.html',data,'utf8');
-})
+    if (er){return console.log("error: ",er);}
+    console.log("ok");
+    writeFileSync("./test/result.html",data,"utf8");
+});
